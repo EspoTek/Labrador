b2eea722edbd9dead513ae86307824eaa50faf02
EspoTek <admin@espotek.com>
2017-07-14 08:41:22 +1000

    V.10 pushed to Android

--- Desktop_Interface/android-build/build/generated/source/buildConfig/release/org/qtproject/example/Labrador/BuildConfig.java
+++ Desktop_Interface/android-build/build/generated/source/buildConfig/release/org/qtproject/example/Labrador/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "org.qtproject.example.Labrador";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 4;
+  public static final int VERSION_CODE = 5;
   public static final String VERSION_NAME = "";
 }
