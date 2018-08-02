634a046097bce24b21346b12494146009b6938a3
Chris Esposito <admin@espotek.com>
2018-08-02 18:27:09 +1000

    more i2 skeleton

--- Desktop_Interface/i2cdecoder.cpp
+++ Desktop_Interface/i2cdecoder.cpp
@@ -1,21 +1,27 @@
 #include "i2cdecoder.h"
 
-i2cDecoder::i2cDecoder(isoBuffer* sda_in, isoBuffer* scl_in) : QObject(nullptr)
+i2cDecoder::i2cDecoder(isoBuffer* sda_in, isoBuffer* scl_in, uint32_t clockRate) : QObject(nullptr)
 {
 	sda = sda_in;
 	scl = scl_in;
+	setStepSize(clockRate);
 }
 
 void i2cDecoder::run()
 {
-	
+	while (serialDistance(sda) > SERIAL_DELAY * sda->sampleRate_bit)
+	{
+		updateBitValues();
+		runStateMachine();
+		serialPtr_bit += stepSize;
+	}	
 } 
 
 int i2cDecoder::serialDistance(isoBuffer* buffer)
 {
     int back_bit = buffer->back * 8;
     int bufferEnd_bit = buffer->bufferEnd * 8;
-    if(back_bit >= serialPtr_bit)
+    if (back_bit >= serialPtr_bit)
         return back_bit - serialPtr_bit;
     else
 		return bufferEnd_bit - serialPtr_bit + back_bit;
@@ -29,4 +35,16 @@ void i2cDecoder::updateBitValues(){
     unsigned char mask = (1 << coord_bit);
 	currentSdaValue = dataByteSda & mask;
 	currentSclValue = dataByteScl & mask;
+}
+
+void i2cDecoder::setStepSize(uint32_t clockRate)
+{
+	stepSize = (double)((sda->sampleRate_bit)/clockRate)/2.0;
+	if (stepSize > (SERIAL_DELAY * sda->sampleRate_bit)/2)
+		stepSize = SERIAL_DELAY * sda->sampleRate_bit / 2;
+}
+
+void i2cDecoder::runStateMachine()
+{
+
 }
--- Desktop_Interface/i2cdecoder.h
+++ Desktop_Interface/i2cdecoder.h
@@ -8,16 +8,19 @@ class i2cDecoder : public QObject
 {
     Q_OBJECT
 public:
-    explicit i2cDecoder(isoBuffer* sda_in, isoBuffer* scl_in);
-    void run(); 
-    int serialDistance(isoBuffer* buffer);
+    explicit i2cDecoder(isoBuffer* sda_in, isoBuffer* scl_in, uint32_t clockRate);
 private:
     isoBuffer* sda;
 	isoBuffer* scl;
 	uint8_t currentSdaValue;
 	uint8_t currentSclValue;
-    int serialPtr_bit = 0;
+    uint64_t serialPtr_bit = 0;
 	void updateBitValues();
+	uint32_t stepSize;
+	void setStepSize(uint32_t clockRate);
+	void runStateMachine();
+    void run(); 
+    int serialDistance(isoBuffer* buffer);
 signals:
 public slots:
 };
