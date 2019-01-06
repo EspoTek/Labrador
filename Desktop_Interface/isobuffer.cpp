4f0c81a157fa7e481cc3508adc9975a8107e7be6
SebastianMestre <mestresebastianunr@gmail.com>
2019-01-06 02:07:47 -0300

    Add a few utility functions to isoBuffer, and reorganize and comment isobuffer.h

--- Desktop_Interface/isobuffer.cpp
+++ Desktop_Interface/isobuffer.cpp
@@ -32,6 +32,55 @@ void isoBuffer::openFile(QString newFile)
     }
 }
 
+void isoBuffer::insertIntoBuffer(short item)
+{
+	buffer[back] = item;
+	back++;
+	if (back > bufferEnd)
+	{
+		back = 0;
+		firstTime = false;
+	}
+}
+
+short isoBuffer::bufferAt(int idx)
+{
+	return buffer[back - idx];
+}
+
+bool isoBuffer::maybeOutputSampleToFile(double convertedSample)
+{
+	average_sample_temp += convertedSample;
+	fileIO_sampleCount++;
+
+	//Check to see if we can write a new sample to file
+	if(fileIO_sampleCount == fileIO_maxIncrementedSampleValue){
+		char numStr[32];
+		sprintf(numStr,"%7.5f, ", average_sample_temp/((double)fileIO_maxIncrementedSampleValue));
+		currentFile->write(numStr);
+		currentColumn++;
+		if (currentColumn >= COLUMN_BREAK){
+		    currentFile->write("\n");
+		    currentColumn = 0;
+		}
+
+		//Reset the average and sample count for next data point
+		fileIO_sampleCount = 0;
+		average_sample_temp = 0;
+
+		//Check to see if we've reached the max file size.
+		if(fileIO_max_file_size != 0){ //value of 0 means "no limit"
+		    fileIO_numBytesWritten += 9;  //7 chars for the number, 1 for the comma and 1 for the space = 9 bytes per sample.
+		    if(fileIO_numBytesWritten >= fileIO_max_file_size){
+		        fileIOEnabled = false; //Just in case signalling fails.
+		        fileIOinternalDisable();
+				return false;
+		    }
+		}
+	}
+	return true;
+}
+
 void isoBuffer::writeBuffer_char(char* data, int len)
 {
     double convertedSample;
--- Desktop_Interface/isobuffer.h
+++ Desktop_Interface/isobuffer.h
@@ -1,6 +1,7 @@
 #ifndef ISOBUFFER_H
 #define ISOBUFFER_H
 
+// TODO: Move headers used only in implementation to isobuffer.cpp
 #include <QWidget>
 #include <QString>
 #include <QByteArray>
@@ -18,46 +19,77 @@ class isoDriver;
 class uartStyleDecoder;
 enum class UartParity : uint8_t;
 
-//isoBuffer is a generic class that enables O(1) read times (!!!) on all read/write operations, while maintaining a huge buffer size.
-//Imagine it as a circular buffer, but with access functions specifically designed for isochronous data from an Xmega.
+// isoBuffer is a generic class that enables O(1) read times (!!!) on all
+// read/write operations, while maintaining a huge buffer size.
+// Imagine it as a circular buffer, but with access functions specifically
+// designed for isochronous data from an Xmega.
 
 #define CONSOLE_UPDATE_TIMER_PERIOD (ISO_PACKETS_PER_CTX * 4)
 
+// TODO: Make private what should be private
 class isoBuffer : public QWidget
 {
     Q_OBJECT
 public:
     isoBuffer(QWidget *parent = 0, int bufferLen = 0, isoDriver *caller = 0, unsigned char channel_value = 0);
-    //Generic Functions
+    // TODO?: Add a destructor
+	
+	//Generic Functions
     void openFile(QString newFile);
-    void writeBuffer_char(char *data, int len);
-    void writeBuffer_short(short *data, int len);
-    short *readBuffer(double sampleWindow, int numSamples, bool singleBit, double delayOffset);
+
+//	Basic buffer operations
+	short bufferAt(int idx);
+	void insertIntoBuffer(short item);
     void clearBuffer();
     void gainBuffer(int gain_log);
-    void glitchInsert(short type);
+    void glitchInsert(short type); // NO-OP
+	
+// Advanced buffer operations
+	void writeBuffer_char(char *data, int len);
+    void writeBuffer_short(short *data, int len);
+
+    short *readBuffer(double sampleWindow, int numSamples, bool singleBit, double delayOffset);
+	
+//	file I/O
+	bool maybeOutputSampleToFile(double convertedSample);
     double sampleConvert(short sample, int TOP, bool AC);
     short inverseSampleConvert(double voltageLevel, int TOP, bool AC);
-    int cap_x0fromLast(double seconds, double vbot);
+
+	int cap_x0fromLast(double seconds, double vbot);
     int cap_x1fromLast(double seconds, int x0, double vbot);
     int cap_x2fromLast(double seconds, int x1, double vtop);
     void serialManage(double baudRate, int type, UartParity parity);
-    //Generic Vars
+
+// ---- MEMBER VARIABLES ----
+
+//	Presentantion?
     QPlainTextEdit *console1, *console2;
     bool serialAutoScroll = true;
     unsigned char channel = 255;
+
+// Conversion And Sampling
     double voltage_ref = 1.65;
     double frontendGain = (R4 / (R3 + R4));
     int samplesPerSecond;
     int sampleRate_bit;
-    int bufferEnd, back = 0;
-    short *buffer, *readData = NULL;
-    uartStyleDecoder *decoder = NULL;
+
+//	Internal Storage
+	int bufferEnd, back = 0;
+    short *buffer;
+
+	short *readData = NULL;
+
+//	UARTS decoding
+	uartStyleDecoder *decoder = NULL;
+	// TODO: change this to keepDecoding
     bool stopDecoding = false;
 private:
-    //Generic Vars
-    bool firstTime = true;
-    //File I/O
+    
+//	Represents wether or not we have already gone around the buffer
+	// NOTE: Appears to be unused: maybe it should be removed?
+	bool firstTime = true;
+
+//	File I/O
     bool fileIOEnabled = false;
     FILE* fptr = NULL;
     QFile *currentFile;
@@ -65,7 +97,6 @@ private:
     int fileIO_sampleCount;
     qulonglong fileIO_max_file_size;
     qulonglong fileIO_numBytesWritten;
-    //isoDriver *parent;
     unsigned int currentColumn = 0;
     isoDriver *virtualParent;
     double average_sample_temp;
